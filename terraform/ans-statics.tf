59efce5975804b5a50f4eed3f8c84a0e05cb0277
Rachel Falder <59665717+rachelf42@users.noreply.github.com>
2025-05-19 14:49:18 -0700

    helps if u stage all changes

--- terraform/ans-statics.tf
+++ terraform/ans-statics.tf
@@ -1,7 +1,7 @@
 ### NETWORK INFRASTRUCTURE
 resource "cloudflare_dns_record" "pve" {
   content = "10.69.69.1" # TODO: setup some kinda loadbalancer for future expansion
-                         # label: waiting
+                         # labels: waiting
                          # Issue URL: https://github.com/rachelf42/homelab/issues/25
   name    = "pve.local.${var.cf_domain}"
   proxied = false
