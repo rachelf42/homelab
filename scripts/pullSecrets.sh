caf54482c69e2892eef5e2e6b21ac23275b83d81
Rachel Falder <59665717+rachelf42@users.noreply.github.com>
2025-05-23 15:36:57 -0700

    fine screw the tabs

--- scripts/pullSecrets.sh
+++ scripts/pullSecrets.sh
@@ -2,12 +2,10 @@
 # TODO: change to pull from NAS
 # Issue URL: https://github.com/rachelf42/homelab/issues/48
 # labels: waiting, hideFromCodeEditor
-cat <<EOD
 rsync \
-	--rsh "ssh \
-		-o StrictHostKeyChecking=no \
-		-o UserKnownHostsFile=/dev/null \
-		-i $HOMELAB_JENKINS_SECRETSYNC_KEY" \
-	--archive --verbose --compress \
-	"$HOMELAB_JENKINS_SECRETSYNC_USER"@rachel-pc.local.rachelf42.ca:/home/rachel/homelab/secrets/ secrets
-EOD
+--rsh "ssh \
+-o StrictHostKeyChecking=no \
+-o UserKnownHostsFile=/dev/null \
+-i $HOMELAB_JENKINS_SECRETSYNC_KEY" \
+--archive --verbose --compress \
+"$HOMELAB_JENKINS_SECRETSYNC_USER"@rachel-pc.local.rachelf42.ca:/home/rachel/homelab/secrets/ secrets
