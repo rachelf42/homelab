39e904212cd9ece89bd5dc087dbced77d3f44b34
Rachel Falder <59665717+rachelf42@users.noreply.github.com>
2025-05-23 15:42:14 -0700

    was it the double indent? guess we dont need it

--- scripts/pullSecrets.sh
+++ scripts/pullSecrets.sh
@@ -3,9 +3,6 @@
 # Issue URL: https://github.com/rachelf42/homelab/issues/48
 # labels: waiting, hideFromCodeEditor
 rsync \
---rsh "ssh \
--o StrictHostKeyChecking=no \
--o UserKnownHostsFile=/dev/null \
--i $HOMELAB_JENKINS_SECRETSYNC_KEY" \
---archive --verbose --compress \
-"$HOMELAB_JENKINS_SECRETSYNC_USER"@rachel-pc.local.rachelf42.ca:/home/rachel/homelab/secrets/ secrets
+	--rsh "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i $HOMELAB_JENKINS_SECRETSYNC_KEY" \
+	--archive --verbose --compress \
+	"$HOMELAB_JENKINS_SECRETSYNC_USER"@rachel-pc.local.rachelf42.ca:/home/rachel/homelab/secrets/ secrets
