b82c352cd470e52125cde45864e872f97f65c1dc
Rachel Falder <59665717+rachelf42@users.noreply.github.com>
2025-05-23 19:40:35 -0700

    maybe it needs output too??

--- terraform/webhookrelay.tf
+++ terraform/webhookrelay.tf
@@ -3,3 +3,9 @@ resource "webhookrelay_bucket" "github-jenkins" {
   description          = "Webhook fired by GitHub to alert Jenkins to new pushes"
   delete_default_input = true
 }
+resource "webhookrelay_output" "github-jenkins" {
+    name = "github-jenkins"
+    destination = "http://jenkins.local.rachelf42.ca:8080/github-webhook/"
+    internal = true
+    bucket_id = webhookrelay_bucket.github-jenkins.id
+}
