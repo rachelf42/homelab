bf2e07f6c03d6bb9412e3a9ba6dda4abab0209cb
Rachel Falder <59665717+rachelf42@users.noreply.github.com>
2025-05-24 14:53:44 -0700

    ????????

--- scripts/triggerJenkinsBuild.sh
+++ scripts/triggerJenkinsBuild.sh
@@ -1,34 +1,38 @@
 #!/bin/bash
 
-if [[ -n "$JENKINS_HOOK_TOKEN" ]]; then
-	echo "MISSING TOKEN"
-	exit 255
-elif [[ -n "$JENKINS_HOOK_URL" ]]; then
-	echo "MISSING URL"
-	exit 255
-elif [[ "${JENKINS_HOOK_URL:0:4}" != "http" ]]; then # basic sanity check only
-	echo "BAD URL"
+if [[ "$TEST_SECRET" != "boogeraidsfart" ]]; then
 	exit 255
 fi
 
-echo "Sending Webhook"
-RESPONSE=$(curl \
-	--fail-with-body \
-	--silent \
-	--form-string "job=$1" \
-	--form-string "token=$JENKINS_HOOK_TOKEN" \
-	--write-out "%{response_code}" \
-	"$JENKINS_HOOK_URL")
-EXITCODE=$?
-echo "Jenkins says: $RESPONSE"
-echo "cURL says: $EXITCODE"
-if [[ $RESPONSE == 201 ]]; then
-	echo 'Queued Successfully'
-	exit 0
-elif [[ $RESPONSE == 303 ]]; then
-	echo 'Build Already Scheduled'
-	exit 1
-else
-	echo 'Unknown Error'
-	exit 255
-fi
+# if [[ -n "$JENKINS_HOOK_TOKEN" ]]; then
+# 	echo "MISSING TOKEN"
+# 	exit 255
+# elif [[ -n "$JENKINS_HOOK_URL" ]]; then
+# 	echo "MISSING URL"
+# 	exit 255
+# elif [[ "${JENKINS_HOOK_URL:0:4}" != "http" ]]; then # basic sanity check only
+# 	echo "BAD URL"
+# 	exit 255
+# fi
+
+# echo "Sending Webhook"
+# RESPONSE=$(curl \
+# 	--fail-with-body \
+# 	--silent \
+# 	--form-string "job=$1" \
+# 	--form-string "token=$JENKINS_HOOK_TOKEN" \
+# 	--write-out "%{response_code}" \
+# 	"$JENKINS_HOOK_URL")
+# EXITCODE=$?
+# echo "Jenkins says: $RESPONSE"
+# echo "cURL says: $EXITCODE"
+# if [[ $RESPONSE == 201 ]]; then
+# 	echo 'Queued Successfully'
+# 	exit 0
+# elif [[ $RESPONSE == 303 ]]; then
+# 	echo 'Build Already Scheduled'
+# 	exit 1
+# else
+# 	echo 'Unknown Error'
+# 	exit 255
+# fi
