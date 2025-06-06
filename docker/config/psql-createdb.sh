b37e433f21bc86f8681970439cee3f2dedff5b52
Rachel Falder <59665717+rachelf42@users.noreply.github.com>
2025-06-05 22:31:36 -0700

    readability

--- docker/config/psql-createdb.sh
+++ docker/config/psql-createdb.sh
@@ -7,6 +7,7 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
 	CREATE DATABASE authelia;
 	GRANT ALL PRIVILEGES ON DATABASE authelia TO authelia;
 	ALTER DATABASE authelia OWNER TO authelia;
+
 	CREATE USER lldap WITH PASSWORD '$(cat /run/secrets/psql-lldap-password)';
 	CREATE DATABASE lldap;
 	GRANT ALL PRIVILEGES ON DATABASE lldap TO lldap;
