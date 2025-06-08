43f8ec6d798b8163fbb7de6cc2959d5178bb15de
Rachel <59665717+rachelf42@users.noreply.github.com>
2025-06-07 18:58:48 -0700

    new terraform workspace for bootstrapping post-deploy (#68)
    will need to either deploy manually one-at-a-time
    or if thats getting too burdensome make a new ansible playbook about it

--- bootstrap/postdeploy/lldap.tf
+++ bootstrap/postdeploy/lldap.tf
@@ -1,7 +1,34 @@
 data "lldap_groups" "groups" {}
 locals {
-  groups = {for each in data.lldap_groups.groups : lookup(each, "display_name", "UNKNOWN") => each}
+  groups = {for each in data.lldap_groups.groups.groups : each.display_name => each}
 }
-output "groupsFound" {
-  value = local.groups
+resource "lldap_group" "nas_access" {
+  display_name = "NAS Access"
+}
+# MY USER
+resource "lldap_user" "mine" {
+  username = "rachel"
+  email = jsondecode(file("${path.module}/usercreds.json")).email
+  password = jsondecode(file("${path.module}/usercreds.json")).password
+  display_name = "Rachel"
+}
+resource "lldap_user_memberships" "mine" {
+  user_id = lldap_user.mine.id
+  group_ids = [
+    local.groups.lldap_admin.id
+  ]
+}
+# AUTHELIA
+resource "lldap_user" "authelia" {
+  username = "authelia"
+  email = "authelia@localhost"
+  password = file("${path.module}/authelia-ldap-password")
+  display_name = "Authelia System User"
+}
+resource "lldap_user_memberships" "authelia" {
+  user_id = lldap_user.authelia.id
+  group_ids = [
+    local.groups.lldap_strict_readonly.id,
+    lldap_group.nas_access.id
+  ]
 }
