0a5ef8afb2440c002d1f2ea1a3aced24dfa9edb3
Rachel <59665717+rachelf42@users.noreply.github.com>
2025-06-12 19:38:17 -0700

    hopefully finalized authelia config (#70)
    * hopefully finalized authelia config
    
    * forgot to change deploy branch
    
    * remove middleware
    
    causing error, need dozzle available to diagnose
    
    * up log level for testing
    
    * stupid mistake
    
    * oops
    
    * Update authelia.yaml
    
    some deprecated options copied from outdated tutorial
    and one stupid typo
    
    * Update authelia.yaml
    
    add required cookie config
    
    * specify redis uid
    
    * redis needs a dir premade
    
    * remove no-start profile
    
    * can successfully log in, adding middleware back in
    
    * helps if i finish the acl
    
    * group changes
    
    * should be good now?
    
    * Update authelia.yaml
    
    * move dozzle to admin subdomain
    
    if this means my nonadmin user cant access anymore we're good to merge
    
    * welp
    
    authelia must have implemented this option since ibracorp did their tutorial
    
    * took out one too many lines
    
    * ????
    
    filter looks broken in the docs mayyyyybe its broken here too?
    
    * ????????
    
    maybe this line? lldaps github example includes it
    
    * attributes maybe??
    
    * revert to 80c72684a7ad119bb694f878196f3e0d95ad94ea
    
    * forgot executable bit
    
    * is it this?
    
    * or this?
    
    * lets try a user filter instead of groups
    
    * screw it, no ldap
    if this works ill remove the comments and merge
    
    * fix failing gh-side linting
    
    * forgot a line
    
    * ansible-lint doesnt like the file missing
    
    * forgot another line
    also users database should be readonly
    
    * stupid indent mistake
    also options moved cause again ibracorp tutorial is old
    
    * i s2g if it was a missing deny rule the whole time
    
    * ready 2 merge

--- bootstrap/postdeploy/lldap.tf
+++ bootstrap/postdeploy/lldap.tf
@@ -3,7 +3,13 @@ locals {
   groups = {for each in data.lldap_groups.groups.groups : each.display_name => each}
 }
 resource "lldap_group" "nas_access" {
-  display_name = "NAS Access"
+  display_name = "nas_access"
+}
+resource "lldap_group" "users" {
+  display_name = "all_users"
+}
+resource "lldap_group" "admin" {
+  display_name = "admin"
 }
 # MY USER
 resource "lldap_user" "mine" {
@@ -15,7 +21,10 @@ resource "lldap_user" "mine" {
 resource "lldap_user_memberships" "mine" {
   user_id = lldap_user.mine.id
   group_ids = [
-    local.groups.lldap_admin.id
+    local.groups.lldap_admin.id,
+    lldap_group.admin.id,
+    lldap_group.users.id,
+    lldap_group.nas_access.id
   ]
 }
 # AUTHELIA
@@ -28,7 +37,6 @@ resource "lldap_user" "authelia" {
 resource "lldap_user_memberships" "authelia" {
   user_id = lldap_user.authelia.id
   group_ids = [
-    local.groups.lldap_strict_readonly.id,
-    lldap_group.nas_access.id
+    local.groups.lldap_strict_readonly.id
   ]
 }
