43db3014fedaf831240f4ac71f8bd58d02077b4a
Rachel Falder <59665717+rachelf42@users.noreply.github.com>
2025-06-15 14:26:57 -0700

    add proxmox api key for homepage cherrypicked from homepage branch cause i need the secret to test

--- terraform/px-settings.tf
+++ terraform/px-settings.tf
@@ -22,3 +22,16 @@ resource "proxmox_virtual_environment_user" "mine" {
     role_id   = "Administrator"
   }
 }
+resource "proxmox_virtual_environment_user_token" "homepage" {
+  token_name      = "homepage"
+  user_id         = proxmox_virtual_environment_user.mine.user_id
+}
+data "proxmox_virtual_environment_role" "auditor" {
+  role_id = "PVEAuditor"
+}
+resource "proxmox_virtual_environment_acl" "homepage" {
+  token_id = proxmox_virtual_environment_user_token.homepage.user_id
+  role_id = data.proxmox_virtual_environment_role.auditor.role_id
+  path      = "/"
+  propagate = true
+}
