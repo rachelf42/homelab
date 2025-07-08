68952dc90dc2f8b9e8a637fe1bc8cf0523296a88
Rachel <59665717+rachelf42@users.noreply.github.com>
2025-07-07 17:49:23 -0700

    Remove jenk agent (#95)
    * removing jenkins agent
    am having issues with ansible i think are being caused by low ram
    since im the only person with push access it should (tm) be fine
    
    * up executors

--- bootstrap/jenkins.tf
+++ bootstrap/jenkins.tf
@@ -56,64 +56,6 @@ resource "proxmox_virtual_environment_vm" "jenkins" {
     up_delay   = 300
   }
 }
-resource "proxmox_virtual_environment_vm" "jenkins-agent" {
-  name          = "jenkins-agent"
-  node_name     = "pve-laptop"
-  description   = "MANAGED BY TERRAFORM"
-  tags          = ["terraform", "jenkins"]
-  timeout_clone = 7200
-  scsi_hardware = "virtio-scsi-single"
-  boot_order    = ["scsi0", "net0"]
-  clone {
-    vm_id = var.proxmox_packer_vmid
-  }
-  lifecycle {
-    precondition {
-      condition     = data.proxmox_virtual_environment_vm.template-vm-ubuntu-noble.template == true && contains(data.proxmox_virtual_environment_vm.template-vm-ubuntu-noble.tags, "packer-template")
-      error_message = "Packer Template Missing!"
-    }
-  }
-  agent {
-    enabled = true
-  }
-  initialization {
-    dns {
-      domain  = "local.rachelf42.ca"
-      servers = [var.dns1, var.dns2]
-    }
-    ip_config {
-      ipv4 {
-        address = "10.69.69.253/8"
-        gateway = "10.0.0.1"
-      }
-    }
-    user_account {
-      username = "jenkins"
-      password = var.secure_password
-    }
-  }
-  cpu {
-    cores   = 2
-    sockets = 1
-    type    = "x86-64-v2-AES"
-  }
-  memory {
-    dedicated = 2048
-    floating  = 2048
-  }
-  network_device {
-    bridge       = "vmbr0"
-    disconnected = false
-    enabled      = true
-    firewall     = true
-    model        = "virtio"
-  }
-  startup {
-    down_delay = 300
-    order      = 1
-    up_delay   = 300
-  }
-}
 resource "ansible_host" "jenkins" {
   name   = "jenkins"
   groups = ["vm", "ubuntu", "jenk", "docker", "server", "jenkcontroller"]
@@ -128,31 +70,9 @@ resource "ansible_host" "jenkins" {
     ansible_user               = "ansible"
   }
 }
-resource "ansible_host" "jenkins-agent" {
-  name   = "jenkins-agent"
-  groups = ["vm", "ubuntu", "jenk", "server", "jenkagent"]
-  variables = {
-    homedir    = "/home/jenkins"
-    repodir    = "/home/jenkins/homelab"
-    ip_address = "10.69.69.254"
-    user_name  = "jenkins"
-
-    ansible_python_interpreter = "/usr/bin/python3"
-    ansible_private_key_file   = "./sshkey"
-    ansible_user               = "ansible"
-  }
-}
 resource "cloudflare_dns_record" "jenkins" {
   name    = "jenkins.local.${var.cf_domain}"
   content = "10.69.69.254"
-  proxied = false
-  ttl     = 1
-  type    = "A"
-  zone_id = local.cf_zone_id
-}
-resource "cloudflare_dns_record" "jenkins-agent" {
-  name    = "jenkins-agent.local.${var.cf_domain}"
-  content = "10.69.69.253"
   proxied = false
   ttl     = 1
   type    = "A"
